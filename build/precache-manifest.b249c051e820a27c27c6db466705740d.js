31ef8f0ecb31321898faef3b3ff97fb743b7d12e
Abdurahman Haydar <your_email_address@example.com>
2020-08-05 07:53:56 +0200

    make

---
+++ build/precache-manifest.b249c051e820a27c27c6db466705740d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d7810013bcb00bb9cbe2aa3285dbd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e096876bb1825b2ca2d",
+    "url": "/static/css/2.192671c0.chunk.css"
+  },
+  {
+    "revision": "2fedb1d2a95975aff577",
+    "url": "/static/css/main.498a9286.chunk.css"
+  },
+  {
+    "revision": "6e096876bb1825b2ca2d",
+    "url": "/static/js/2.0ffd00c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ffd00c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fedb1d2a95975aff577",
+    "url": "/static/js/main.941b3902.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "e0d2b7486b532288a980d0cd5d535340",
+    "url": "/static/media/chat-icon.e0d2b748.svg"
+  },
+  {
+    "revision": "7718b3e3ff59dac728c55e725a3b0523",
+    "url": "/static/media/logo-no-bg.7718b3e3.svg"
+  },
+  {
+    "revision": "0d062b33a8b60acb2a497afe63666b16",
+    "url": "/static/media/notification.0d062b33.mp3"
+  }
+]);
